1db5ffeb6a499d74f0fd58c61a1db19b399eb132
KhansaMahira <khansamahira011@gmail.com>
2023-11-01 14:42:46 +0700

    Library App Tugas 7

---
+++ lib/menu.dart
@@ -0,0 +1,111 @@
+import 'package:flutter/material.dart';
+
+class LibraryItem {
+  final String name;
+  final IconData icon;
+
+  LibraryItem(this.name, this.icon);
+}
+
+class LibraryCard extends StatelessWidget {
+  final LibraryItem item;
+
+  const LibraryCard(this.item, {super.key}); // Constructor
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: Colors.indigo,
+      child: InkWell(
+        // Area responsive terhadap sentuhan
+        onTap: () {
+          // Memunculkan SnackBar ketika diklik
+          ScaffoldMessenger.of(context)
+            ..hideCurrentSnackBar()
+            ..showSnackBar(SnackBar(
+                content: Text("Kamu telah menekan tombol ${item.name}!")));
+        },
+        child: Container(
+          // Container untuk menyimpan Icon dan Text
+          padding: const EdgeInsets.all(8),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(
+                  item.icon,
+                  color: Colors.white,
+                  size: 30.0,
+                ),
+                const Padding(padding: EdgeInsets.all(3)),
+                Text(
+                  item.name,
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(color: Colors.white),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  MyHomePage({Key? key}) : super(key: key);
+
+  final List<LibraryItem> items = [
+    LibraryItem("Lihat Item", Icons.checklist),
+    LibraryItem("Tambah Item", Icons.add_shopping_cart),
+    LibraryItem("Logout", Icons.logout),
+];
+
+    @override
+    Widget build(BuildContext context) {
+        return Scaffold(
+          appBar: AppBar(
+            title: const Text(
+              'Library'
+            ),
+          ),
+          body: SingleChildScrollView(
+            // Widget wrapper yang dapat discroll
+            child: Padding(
+              padding: const EdgeInsets.all(10.0), // Set padding dari halaman
+              child: Column(
+                // Widget untuk menampilkan children secara vertikal
+                children: <Widget>[
+                  const Padding(
+                    padding: EdgeInsets.only(top: 10.0, bottom: 10.0),
+                    // Widget Text untuk menampilkan tulisan dengan alignment center dan style yang sesuai
+                    child: Text(
+                      'PBP Library',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  // Grid layout
+                  GridView.count(
+                    // Container pada card kita.
+                    primary: true,
+                    padding: const EdgeInsets.all(20),
+                    crossAxisSpacing: 10,
+                    mainAxisSpacing: 10,
+                    crossAxisCount: 3,
+                    shrinkWrap: true,
+                    children: items.map((LibraryItem item) {
+                      // Iterasi untuk setiap item
+                      return LibraryCard(item);
+                    }).toList(),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        );
+    }
+}
